3de25835adf7aa26a7439d6d71a3b4216aae2966
alan po <mankeip3531@hstat.org>
2021-08-16 21:20:24 -0400

    com

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className = "app">
         <Route exact path="/" component={Home}/>
         <Route path="/task" component={Task}/>
+        <Route path="*" component={Task}/>
     </div>
   );
 }
